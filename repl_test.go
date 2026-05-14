21c469acaf5c85cf62607db0898dc7e56ed40b75
Shamiul-isl <shamiul.samislam@gmail.com>
2026-05-14 14:43:58 -0400

    added unit test repl

---
+++ repl_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		// add more cases here
+		{
+			input:    "Hello  World",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("unexpected lengths. Expected: %v, Actual: %v", len(actual), len(c.expected))
+			t.Fatal()
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("unexpected results. Expected: %s, Actual: %s", expectedWord, word)
+				t.Fatal()
+			}
+		}
+	}
+}
